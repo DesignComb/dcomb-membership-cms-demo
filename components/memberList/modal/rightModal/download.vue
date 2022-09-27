70cea9cf02c0fc07382f8428c96354bbc7fecfa5
DesignComb <109861003+DesignComb@users.noreply.github.com>
2022-09-27 16:39:26 +0800

    feat: download member list func

--- components/memberList/modal/rightModal/download.vue
+++ components/memberList/modal/rightModal/download.vue
@@ -17,6 +17,14 @@ export default {
   mixins: [createCsvFile],
   methods: {
     async downloadMembers() {
+      const tmp = this.$store.state.memberList.members
+      const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
+      const header = Object.keys(tmp[0])
+      const csv = [
+        header.join(','), // header row first
+        ...tmp.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+      ].join('\r\n')
+      await this.createCsvFile(csv)
     }
   },
 }
